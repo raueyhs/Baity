236eb64d1d7f56186c940ae8187832e1177b2993
shyeuar <w2654896@outlook.com>
2025-10-13 13:45:37 +0800

    fix some issue

---
+++ src/main/java/com/shyeuar/baity/config/Config.java
@@ -0,0 +1,28 @@
+package com.shyeuar.baity.config;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class Config {
+
+    // 鍏朵粬妯″潡寮€鍏?
+    public static final boolean ENABLE_FUN_MODULES = true;
+    public static final boolean ENABLE_RENDER_MODULES = true;
+    public static final boolean ENABLE_HUD_MODULES = true;
+    
+    // 鐗堟湰绫诲瀷
+    public static final String VERSION_TYPE = 
+        System.getProperty("baity.version.type", "open-source");
+    
+    // 璋冭瘯淇℃伅
+    public static void printConfig() {
+        System.out.println("[Baity] 閰嶇疆淇℃伅:");
+        System.out.println("  - 鐗堟湰绫诲瀷: " + VERSION_TYPE);
+        
+        System.out.println("  - Fun妯″潡: " + (ENABLE_FUN_MODULES ? "鍚敤" : "绂佺敤"));
+        System.out.println("  - Render妯″潡: " + (ENABLE_RENDER_MODULES ? "鍚敤" : "绂佺敤"));
+        System.out.println("  - HUD妯″潡: " + (ENABLE_HUD_MODULES ? "鍚敤" : "绂佺敤"));
+    }
+}
+
