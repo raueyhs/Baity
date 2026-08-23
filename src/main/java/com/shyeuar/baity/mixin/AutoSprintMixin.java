054f61194e84a94ec7bd85d2cbb063c330cd92c9
shyeuar <w2654896@outlook.com>
2026-08-24 01:28:09 +0800

    ported AutoSprint refactor from 26.2 (#22)

--- src/main/java/com/shyeuar/baity/mixin/AutoSprintMixin.java
+++ src/main/java/com/shyeuar/baity/mixin/AutoSprintMixin.java
@@ -1,17 +1,26 @@
 package com.shyeuar.baity.mixin;
 
-import com.shyeuar.baity.features.AutoSprint;
-import net.minecraft.client.KeyMapping;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.shyeuar.baity.config.ConfigManager;
+import net.minecraft.client.player.LocalPlayer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(KeyMapping.class)
+@Mixin(LocalPlayer.class)
 public class AutoSprintMixin {
 
-    @Inject(method = "isDown", at = @At("HEAD"), cancellable = true)
-    private void baity$autoSprint(CallbackInfoReturnable<Boolean> cir) {
-        AutoSprint.handleSprintKeyIsDown((KeyMapping) (Object) this, cir);
+    @ModifyExpressionValue(
+        method = "aiStep",
+        at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Input;sprint()Z"),
+        require = 1
+    )
+    private boolean baity$autoSprint(boolean original) {
+        if (!ConfigManager.autoSprintEnabled) {
+            return original;
+        }
+        if (!ConfigManager.autoSprintUnderWater && ((LocalPlayer) (Object) this).isInWater()) {
+            return original;
+        }
+        return true;
     }
 }
